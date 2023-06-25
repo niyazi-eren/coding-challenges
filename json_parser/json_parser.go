de0b363cb73e5d3432dfa4595aa0691134e68b4a
neren <neren@sqli.com>
2023-06-25 13:59:01 +0200

    json parser part 2

--- json_parser/json_parser.go
+++ json_parser/json_parser.go
@@ -6,29 +6,85 @@ import (
 	"os"
 )
 
-func Validate(fileName string) bool {
-	var tokens = make([]string, 0, 100)
-	file, err := os.Open(fileName)
-	if err != nil {
-		return false
-	}
+const (
+	stringDelim  = "\""
+	kvDelim      = ":"
+	start        = "{"
+	end          = "}"
+	arrayStart   = "["
+	arrayEnd     = "]"
+	elementDelim = ","
+	space        = " "
+	newLine      = "\n"
+)
 
+func isValidJsonFile(fileName string) bool {
+	tokens := tokenize(fileName)
+	return len(tokens) >= 2 &&
+		tokens[0] == start &&
+		tokens[len(tokens)-1] == end &&
+		isValidKeyValuePair(tokens)
+}
+
+func isForbiddenToken(token string) bool {
+	values := []string{kvDelim, start, end, arrayStart, arrayEnd, elementDelim}
+	for _, val := range values {
+		if token == val {
+			return true
+		}
+	}
+	return false
+}
+
+func isValidKeyValuePair(tokens []string) bool {
+	if !(len(tokens) > 2) {
+		return true
+	}
+	for i := 1; i < len(tokens)-1; i++ {
+		if ((i%4 == 1 || i%4 == 3) && isForbiddenToken(tokens[i])) ||
+			(i%4 == 2 && (tokens[i] != kvDelim)) ||
+			(i%4 == 0 && tokens[i] != elementDelim) ||
+			// edge case where last token is a comma
+			(i%4 == 0 && (i+4 > len(tokens)-1) && tokens[i] == elementDelim) {
+			return false
+		}
+	}
+	return true
+}
+
+func tokenize(fileName string) []string {
+	file, _ := os.Open(fileName)
+	defer file.Close()
+
+	var tokens = make([]string, 0, 100)
+	parsingString := false
 	reader := bufio.NewReader(file)
+
+	token := ""
 	for {
-		char, _, err := reader.ReadRune()
+		ch, _, err := reader.ReadRune()
+		char := string(ch)
+
 		if err != nil && err.Error() != "EOF" {
 			fmt.Println("Error reading file:", err)
-			return false
 		}
 		if err != nil && err.Error() == "EOF" {
 			break
 		}
-		token := string(char)
-		tokens = append(tokens, token)
+
+		// handle key value pairs for string type
+		if char == stringDelim && !parsingString {
+			parsingString = true
+			continue
+		} else if char == stringDelim && parsingString {
+			parsingString = false
+			tokens = append(tokens, token)
+			token = ""
+		} else if parsingString && char != stringDelim {
+			token += char
+		} else if char != space && char != newLine {
+			tokens = append(tokens, char)
+		}
 	}
-
-	defer file.Close()
-
-	return len(tokens) >= 2 && tokens[0] == "{" && tokens[len(tokens)-1] == "}"
-
+	return tokens
 }
--- json_parser/json_parser_test.go
+++ json_parser/json_parser_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestStepOneValid(t *testing.T) {
-	result := Validate("tests/step1/valid.json")
+	result := isValidJsonFile("tests/step1/valid.json")
 	expected := true
 	if result != expected {
 		t.Errorf("result is %v; expected %v", result, expected)
@@ -13,7 +13,39 @@ func TestStepOneValid(t *testing.T) {
 }
 
 func TestStepOneInvalid(t *testing.T) {
-	result := Validate("tests/step1/invalid.json")
+	result := isValidJsonFile("tests/step1/invalid.json")
+	expected := false
+	if result != expected {
+		t.Errorf("result is %v; expected %v", result, expected)
+	}
+}
+
+func TestStepTwoValid(t *testing.T) {
+	result := isValidJsonFile("tests/step2/valid.json")
+	expected := true
+	if result != expected {
+		t.Errorf("result is %v; expected %v", result, expected)
+	}
+}
+
+func TestStepTwoValid2(t *testing.T) {
+	result := isValidJsonFile("tests/step2/valid2.json")
+	expected := true
+	if result != expected {
+		t.Errorf("result is %v; expected %v", result, expected)
+	}
+}
+
+func TestStepTwoInvalid(t *testing.T) {
+	result := isValidJsonFile("tests/step2/invalid.json")
+	expected := false
+	if result != expected {
+		t.Errorf("result is %v; expected %v", result, expected)
+	}
+}
+
+func TestStepTwoInvalid2(t *testing.T) {
+	result := isValidJsonFile("tests/step2/invalid2.json")
 	expected := false
 	if result != expected {
 		t.Errorf("result is %v; expected %v", result, expected)
