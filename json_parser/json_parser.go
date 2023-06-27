57ba9f014de7498ea79a0461436248e848f5bdbf
neren <neren@sqli.com>
2023-06-27 17:23:23 +0200

    json parser part 5 invalid cases refactor

--- json_parser/json_parser.go
+++ json_parser/json_parser.go
@@ -4,7 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
+	"regexp"
 	"strings"
 	"unicode"
 )
@@ -45,6 +45,70 @@ func getContent(fileName string) string {
 	return content
 }
 
+func tokenize(content string) []string {
+	tokens := make([]string, 0, 200)
+	var parsingString, parsingObject, parsingArray bool
+	var token string
+
+	for _, ch := range content {
+		char := string(ch)
+
+		switch {
+		case parsingObject:
+			if char == end {
+				parsingObject = false
+				token += char
+				tokens = append(tokens, token)
+				token = ""
+			} else if char == space || char == newLine {
+				continue
+			} else {
+				token += char
+			}
+		case parsingArray:
+			if char == arrayEnd {
+				parsingArray = false
+				token += char
+				tokens = append(tokens, token)
+				token = ""
+			} else {
+				token += char
+			}
+		case char == stringDelim && !parsingString:
+			parsingString = true
+			token += stringDelim
+			continue
+		case parsingString && char != stringDelim:
+			token += char
+		case char == stringDelim && parsingString:
+			parsingString = false
+			token += stringDelim
+			tokens = append(tokens, token)
+			token = ""
+		case char == space || char == newLine:
+			continue
+		case isSpecialToken(char):
+			if char == start && len(tokens) == 0 {
+				tokens = append(tokens, char)
+			} else if char == start {
+				parsingObject = true
+				token += char
+			} else if char == arrayStart {
+				parsingArray = true
+				token += char
+			} else if token != "" {
+				tokens = append(tokens, token, char)
+				token = ""
+			} else {
+				tokens = append(tokens, char)
+			}
+		default:
+			token += char
+		}
+	}
+	return tokens
+}
+
 func isSpecialToken(token string) bool {
 	values := []string{kvDelim, start, end, elementDelim, arrayStart, arrayEnd}
 	for _, val := range values {
@@ -53,6 +117,29 @@ func isSpecialToken(token string) bool {
 		}
 	}
 	return false
+}
+
+func isValidKeyValuePair(tokens []string) bool {
+	if len(tokens) == 2 {
+		return true
+	}
+	for i := 1; i < len(tokens)-1; i++ {
+		// validate key
+		if (i%4 == 1 && !isValidKey(tokens[i])) ||
+			// validate delimiter
+			(i%4 == 2 && (tokens[i] != kvDelim)) ||
+			// check if is an inner object
+			(i%4 == 3 && (string(tokens[i][0]) == start) && !isValidKeyValuePair(tokenize(tokens[i]))) ||
+			// validate value otherwise
+			(i%4 == 3 && (string(tokens[i][0]) != start) && !isValidValue(tokens[i])) ||
+			// validate element delimiter
+			(i%4 == 0 && tokens[i] != elementDelim) ||
+			// edge case where last token is a comma
+			(i%4 == 0 && (i+4 > len(tokens)-1) && tokens[i] == elementDelim) {
+			return false
+		}
+	}
+	return true
 }
 
 func isValidKey(token string) bool {
@@ -70,13 +157,9 @@ func isValidValue(token string) bool {
 	if token[0] == '"' && token[len(token)-1] == '"' {
 		return true
 	}
-	// case number
-	if unicode.IsDigit(rune(token[0])) {
-		if string(token[0]) == "0" {
-			return false
-		}
-		_, err := strconv.Atoi(token)
-		return err == nil
+
+	if unicode.IsDigit(rune(token[0])) || (rune(token[0]) == '-') {
+		return isValidNumber(token)
 	}
 
 	// case array
@@ -84,6 +167,9 @@ func isValidValue(token string) bool {
 		// split array by values
 		values := strings.Split(token[1:len(token)-1], elementDelim)
 		for _, value := range values {
+			// trim spaces
+			value = strings.TrimSpace(value)
+
 			if value != "" && !(isValidValue(value)) {
 				return false
 			}
@@ -94,107 +180,24 @@ func isValidValue(token string) bool {
 	return isBoolean(token) || isNull(token)
 }
 
+func isValidNumber(token string) bool {
+	// case with leading zeros
+	if len(token) > 2 && token[0] == '0' && !(token[1] == 'e' || token[1] == 'E' || token[1] == '-' || token[1] == '.') {
+		return false
+	}
+	exclude, _ := regexp.MatchString("^\\d+[+-]\\d+$", token)
+	if exclude {
+		return false
+	}
+
+	match, _ := regexp.MatchString("([-+]?\\d+(\\.\\d+)?([eE][-+]?\\d+)?|\\d+\n)", token)
+	return match
+}
+
 func isBoolean(token string) bool {
 	return token == "true" || token == "false"
 }
 
 func isNull(token string) bool {
 	return token == "null"
-}
-
-func isValidKeyValuePair(tokens []string) bool {
-	if len(tokens) == 2 {
-		return true
-	}
-	for i := 1; i < len(tokens)-1; i++ {
-		// validate key
-		if (i%4 == 1 && !isValidKey(tokens[i])) ||
-
-			// validate delimiter
-			(i%4 == 2 && (tokens[i] != kvDelim)) ||
-
-			// validate value
-			// check if is an inner object
-			(i%4 == 3 && (string(tokens[i][0]) == start) && !isValidKeyValuePair(tokenize(tokens[i]))) ||
-			(i%4 == 3 && (string(tokens[i][0]) != start) && !isValidValue(tokens[i])) ||
-
-			// validate element delimiter
-			(i%4 == 0 && tokens[i] != elementDelim) ||
-			// edge case where last token is a comma
-			(i%4 == 0 && (i+4 > len(tokens)-1) && tokens[i] == elementDelim) {
-			return false
-		}
-	}
-	return true
-}
-
-func tokenize(content string) []string {
-	var tokens = make([]string, 0, 100)
-	parsingString := false
-	firstStart := true
-	parsingInnerObject := false
-	parsingArray := false
-
-	token := ""
-	for _, ch := range content {
-		char := string(ch)
-
-		if parsingInnerObject == true {
-			if char == end {
-				parsingInnerObject = false
-				token += char
-				tokens = append(tokens, token)
-				token = ""
-			} else if char == space || char == newLine {
-				continue
-			} else {
-				token += char
-			}
-		} else if parsingArray {
-			if char == arrayEnd {
-				parsingArray = false
-				token += char
-				tokens = append(tokens, token)
-				token = ""
-			} else {
-				token += char
-			}
-		} else if char == stringDelim && !parsingString {
-			parsingString = true
-			token += stringDelim
-			continue
-			// append to string
-		} else if parsingString && char != stringDelim {
-			token += char
-			// finished parsing string
-		} else if char == stringDelim && parsingString {
-			parsingString = false
-			token += stringDelim
-			tokens = append(tokens, token)
-			token = ""
-		} else if char == space || char == newLine {
-			continue
-		} else if !isSpecialToken(char) {
-			token += char
-		} else if isSpecialToken(char) {
-			// if first start
-			if char == start && firstStart {
-				tokens = append(tokens, char)
-				firstStart = false
-			} else if char == start && !firstStart {
-				parsingInnerObject = true
-				token += char
-			} else if char == arrayStart {
-				parsingArray = true
-				token += char
-			} else if token != "" {
-				tokens = append(tokens, token)
-				token = ""
-				tokens = append(tokens, char)
-			} else {
-				tokens = append(tokens, char)
-			}
-		}
-	}
-	return tokens
 }
